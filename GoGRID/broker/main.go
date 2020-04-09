83a719daa6f0d0c4ca024ff9298f597162d7300c
Macularius <igor.kovatsenko@yandex.ru>
2020-04-09 22:51:18 +0400

    каркас примера исполняемого кода

--- GoGRID/broker/main.go
+++ GoGRID/broker/main.go
@@ -1,34 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"grid/GoGRID/broker/core"
 	"grid/GoGRID/broker/optimizer"
 )
 
 func main() {
 	var (
-		// book   string
-		// substr string
+		book   string
+		substr string
 
 		taskCount int
 
-		// chTasks chan string
+		chTasks chan string
 	)
 
-	// book, substr = GetData()
+	book, substr = GetData()
 
 	taskCount = optimizer.Optimize2(12391238, 20)
 
-	fmt.Println(taskCount)
-	return
-
-	// chTasks = core.BrokeAsync(book, substr, taskCount)
+	chTasks = core.BrokeAsync(book, substr, taskCount)
 
 	for {
 		select {
-		// case task := <-chTasks:
-		// задача в распределитель
-
+		case task := <-chTasks:
+			// задача в распределитель
+			
 		}
 	}
 }
