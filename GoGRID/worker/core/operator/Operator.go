df3d6d6c839745cdf11019bf95390fc1269400fd
Brainiack5 <35738996+Brainiack5@users.noreply.github.com>
2020-04-10 23:26:33 +0400

    worker.operator listener

--- GoGRID/worker/core/operator/Operator.go
+++ GoGRID/worker/core/operator/Operator.go
@@ -3,9 +3,11 @@ package operator
 import (
 	"fmt"
 	"grid/GoGRID/worker/core/settings"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
+	"os/exec"
 )
 
 // Operator получатель и отправитель сообщений
@@ -61,10 +63,56 @@ func (o *Operator) Listener() (err error) {
 
 func solution(w http.ResponseWriter, r *http.Request) {
 
-	// r.PostForm.Get("token", string(token))
-	// r.PostForm.Get("task_id", strconv.Itoa(t.ID))
-	// r.PostForm.Get("task_body", string(t.Body))
-	// r.PostForm.Get("task_workcode", string(t.WorkCode))
+	var (
+		err           error
+		token         string
+		task_id       string
+		task_body     string
+		task_workcode string
+		URL           = net.JoinHostPort(settings.Config.DistributorHost, settings.Config.DistributorPort) + "/solution"
+	)
+	//вытащить из запроса параметры
+	//создать исполняемый файл и запустить его передав параметры:-token, -task_id
+
+	if token = r.PostForm.Get("token"); token == "" {
+		err = fmt.Errorf("Токен не может быть пустым")
+		log.Printf("error operator.solution : r.PostForm.Get, %v\n", err)
+		return
+	}
+
+	if task_id = r.PostForm.Get("task_id"); task_id == "" {
+		err = fmt.Errorf("Идентификационный номер подзадачи отсутствует")
+		log.Printf("error operator.solution : r.PostForm.Get, %v\n", err)
+		return
+	}
+
+	if task_body = r.PostForm.Get("task_body"); task_body == "" {
+		err = fmt.Errorf("Отсутствует тело задачи")
+		log.Printf("error operator.solution : r.PostForm.Get, %v\n", err)
+		return
+	}
+
+	if task_workcode = r.PostForm.Get("task_workcode"); task_workcode == "" {
+		err = fmt.Errorf("Отсутствует исполняемый код")
+		log.Printf("error operator.solution : r.PostForm.Get, %v\n", err)
+		return
+	}
+
+	ioutil.WriteFile("TaskFile.go", []byte(task_body), 0777)
+	if err != nil {
+		log.Printf("error Operator.solution : ioutil.WriteFile, %v\n", err)
+		return
+	}
+
+	cmd := exec.Command("go", "run", "TaskFile.go", "-task_id", task_id, "-token", token, "-URL", URL)
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Print(string(stdout))
 
 	return
 }
