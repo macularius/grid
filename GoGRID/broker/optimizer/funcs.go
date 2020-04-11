505e8be9573d79833533f43706cde72f9f5db0b6
Macularius <igor.kovatsenko@yandex.ru>
2020-04-11 06:22:59 +0400

    it is worked2

--- GoGRID/broker/optimizer/funcs.go
+++ GoGRID/broker/optimizer/funcs.go
@@ -38,17 +38,19 @@ func Optimize(book, substr string) (taskCount int) {
 }
 
 // Optimize2 возвращает оптимальное количество задач для указанных книги и подстроки
-func Optimize2(bookln, substrln int) (taskCount int) {
+func Optimize2(book, substr string) (taskCount int) {
 	var (
-		i float32 // нижняя граница оптимальной длины задачи
+		bookln   = len(book)
+		substrln = len(substr)
+		i        float32 // нижняя граница оптимальной длины задачи
 
 		x int
 	)
 
 	// если книга сильно больше подстроки, то выставляем нижнюю границу оптимально промежутка длины задачи
 	// иначе отношение подстроки к книге
-	if float32(substrln)/float32(bookln) < 0.000152 {
-		i = 0.000152
+	if float32(substrln)/float32(bookln) < 0.00452 {
+		i = 0.00452
 	} else {
 		i = float32(substrln) / float32(bookln)
 	}
@@ -56,16 +58,13 @@ func Optimize2(bookln, substrln int) (taskCount int) {
 	for x <= 0 {
 		x = int(float32(bookln)*i) - substrln // сдвиг
 		if x < 1 {
-			i += 0.00005
+			i += 0.0005
 			continue
 		}
 
 		taskCount = (bookln - substrln) / x
 
 		fmt.Printf("Task count %v, i = %v\n", taskCount, i)
-		// fmt.Printf("float32(bookln)*%v = %v\n", i, int(float32(bookln)*i))
-		fmt.Printf("x = %v\n", x)
-		// fmt.Printf("substrln = %v\n\n", substrln)
 
 		i += 0.00005
 	}
